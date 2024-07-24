a716ab872aae9ae4c31f49f2207e8d3a43688d31
Scott Haley <williamscotthaley@gmail.com>
2024-07-23 22:02:50 -0500

    better nvim-tree navigation

--- lua/shaley/core/keymaps.lua
+++ lua/shaley/core/keymaps.lua
@@ -5,6 +5,7 @@ local keymap = vim.keymap
 keymap.set("i", "fd", "<ESC>", { desc = "Exit insert mode with fd" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>qq", ":qa<CR>", { desc = "Quit all" })
 
 -- window management
 keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertically" })
--- lua/shaley/plugins/nvim-tree.lua
+++ lua/shaley/plugins/nvim-tree.lua
@@ -1,56 +1,106 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  config = function()
-    local nvimtree = require("nvim-tree")
+	"nvim-tree/nvim-tree.lua",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local nvimtree = require("nvim-tree")
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+		local function my_on_attach(bufnr)
+			local api = require("nvim-tree.api")
 
-    nvimtree.setup({
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
-            },
-          },
-        },
-      },
-      -- disable window_picker for
-      -- explorer to work well with
-      -- window splits
-      actions = {
-        open_file = {
-          window_picker = {
-            enable = false,
-          },
-        },
-      },
-      filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      },
-    })
+			local function opts(desc)
+				return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+			end
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+			local function edit_or_open()
+				local node = api.tree.get_node_under_cursor()
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+				if node.nodes ~= nil then
+					-- expand or collapse folder
+					api.node.open.edit()
+				else
+					-- open file
+					api.node.open.edit()
+					-- Close the tree if file was opened
+					api.tree.close()
+				end
+			end
+
+			-- open as vsplit on current node
+			local function vsplit_preview()
+				local node = api.tree.get_node_under_cursor()
+
+				if node.nodes ~= nil then
+					-- expand or collapse folder
+					api.node.open.edit()
+				else
+					-- open file as vsplit
+					api.node.open.vertical()
+				end
+
+				-- Finally refocus on tree if it was lost
+				api.tree.focus()
+			end
+			local keymap = vim.keymap -- for conciseness
+
+			keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
+			keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
+			keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+			keymap.set("n", "H", api.tree.collapse_all, opts("Collapse All"))
+		end
+
+		-- recommended settings from nvim-tree documentation
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		nvimtree.setup({
+			view = {
+				width = 35,
+				relativenumber = true,
+			},
+			on_attach = my_on_attach,
+			-- change folder arrow icons
+			renderer = {
+				indent_markers = {
+					enable = true,
+				},
+				icons = {
+					glyphs = {
+						folder = {
+							arrow_closed = "", -- arrow when folder is closed
+							arrow_open = "", -- arrow when folder is open
+						},
+					},
+				},
+			},
+			-- disable window_picker for
+			-- explorer to work well with
+			-- window splits
+			actions = {
+				open_file = {
+					window_picker = {
+						enable = false,
+					},
+				},
+			},
+			filters = {
+				custom = { ".DS_Store" },
+			},
+			git = {
+				ignore = false,
+			},
+		})
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current file" }
+		) -- toggle file explorer on current file
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
 }
